5b23fac4307d3fb04331ce3d4795212f74b317c5
CodecoolCsibi <jozsef.berko.ifj@gmail.com>
2017-05-22 20:10:05 +0200

    Home address

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -78,6 +78,7 @@ public class MyGdxGame extends ApplicationAdapter {
 			} catch (Exception e){
 				System.out.println("Client: Unable to send data.");
 				e.printStackTrace();
+				System.exit(1);
 				}
 			}
 		}
@@ -110,7 +111,7 @@ public class MyGdxGame extends ApplicationAdapter {
 
 				serverSocketHint.acceptTimeout = 99999999;
 
-				ServerSocket serverSocket = Gdx.net.newServerSocket(Net.Protocol.TCP, "10.5.20.255", 9021, serverSocketHint);
+				ServerSocket serverSocket = Gdx.net.newServerSocket(Net.Protocol.TCP, "192.168.0.100", 9021, serverSocketHint);
 				socket = serverSocket.accept(new SocketHints());
 				System.out.println("Server: Client connected.");
 				String dataLine;
@@ -149,7 +150,7 @@ public class MyGdxGame extends ApplicationAdapter {
 		//create the socket and connect to the server entered in the text box ( x.x.x.x format ) on port 9021
 
 		try {
-			socket = Gdx.net.newClientSocket(Net.Protocol.TCP, "10.5.20.255", 9021, socketHints);
+			socket = Gdx.net.newClientSocket(Net.Protocol.TCP, "192.168.0.100", 9021, socketHints);
 			System.out.println("connection established");
 		} catch (Exception e) {
 			System.exit(1);
